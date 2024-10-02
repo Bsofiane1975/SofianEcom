6b26061ebb3186059ebcb720fa76a0679aec3de5
Boumaiza sofiane <boumaizas75@gmail.com>
2024-10-02 14:49:45 +0100

    creation d'une application ecommerce multiplatforme avec react

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">MULTIPLATEFORME ECOMMERCE </a>
+      </header>
+
+      <main>PRODUCT OF MULTIPLATEFORME ECOMMERCE LISTS</main>
+    </div>
+  );
+}
+
+export default App;
